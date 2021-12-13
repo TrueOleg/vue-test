552cf5f46a441b6122c28c4aa525f5d8969bf97c
Bmsftdeva <>
2021-12-13 05:40:19 +0300

    ghjghj

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -4,21 +4,25 @@
     <p>{{counter}}</p>
 
     <button @click="incrementCounter">increment counter</button>
+    <button @click="decrementCounter">decrement counter</button>
   </div>
 </template>
 
-<script>
-  export default {
-    methods: {
-      incrementCounter () {
-        this.$store.dispatch('inrementAction', 1)
-      }
-    },
-
-    computed: {
-      counter () {
-        return this.$store.getters.counter
-      }
-    }
-  }
+<script lang="js" src="./Counter.vue.js">
+  // export default {
+  //   methods: {
+  //     incrementCounter () {
+  //       this.$store.dispatch('incrementAction', 1)
+  //     },
+  //     decrementCounter() {
+  //       this.$store.dispatch('decrementAction', 1)
+  //     }
+  //   },
+  //
+  //   computed: {
+  //     counter () {
+  //       return this.$store.getters.counter
+  //     }
+  //   }
+  // }
 </script>
